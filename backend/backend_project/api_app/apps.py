0e773782e8d83e4453b1b65724cc7918aca34826
russel-07 <russel170790@gmail.com>
2023-12-08 21:26:48 +0300

    update app

--- backend/backend_project/api_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class BackendBotAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_app'
-    verbose_name = 'usd rate bot'
